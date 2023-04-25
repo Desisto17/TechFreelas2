112c6b880f83c25bdb0f91b6dee1331f6c6997bd
Desisto17 <breno_serra10@hotmail.com>
2023-04-25 16:22:29 -0300

    include profile-index

---
+++ TechFreelas-master/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "inter": "Inter, sans-serif"
+    },
+    extend: {
+      colors: {
+        "oxford-blue": "#0D1530",
+        "box-blue": "#121C40",
+        "rich": "#090E20",
+        "risd": "#3059B0",
+        "cream": "#FAFFD8",
+        "slate-purple": "#6957B2",
+      },
+      gridTemplateColumns: {
+        "body-cols": "1fr",
+      },
+      gridTemplateRows: {
+        "body-rows": "auto 100vh auto",
+      },
+      boxShadow: {
+        "md": "1px 1px 15px #00000085",
+      }
+    },
+  },
+  plugins: [],
+}
+
